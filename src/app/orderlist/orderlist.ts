74f16a79e8766085b7663f287f05f1555d3e858a
Pc-Carlo\Carlo <c.buonfi@gmail.com>
2023-01-05 14:44:57 +0100

    ok

---
+++ src/app/orderlist/orderlist.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Order } from './order';
+import { OrderService } from './orderlist.service';
+
+@Component({
+  selector: 'orderlist',
+  templateUrl: './orderlist.html',
+  styleUrls: ['../app/app.component.css'] //errore?
+})
+
+
+export class OrderlistComponent implements OnInit 
+{
+  orderlist:Order[] = [];
+  constructor(private orderService: OrderService) { }
+  ngOnInit(): void {
+    this.getOrder();
+  }
+  getOrder(): void {
+
+    this.orderService.getOrderList()
+
+    .subscribe(orderlist => this.orderlist = orderlist);
+  }
+}

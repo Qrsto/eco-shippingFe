fc395ffd42fb10ab4c61d23f8e626df5554a361f
Pc-Carlo\Carlo <c.buonfi@gmail.com>
2023-01-05 18:37:45 +0100

    Orden_list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,18 +11,7 @@ import { RegistrautenteComponent } from './registrautente/registrautente.compone
 import { CreazioneordineComponent } from './creazioneordine/creazioneordine.component';
 import { GestioneordineComponent } from './gestioneordine/gestioneordine.component';
 import { PagamentoComponent } from './pagamento/pagamento.component';
-<<<<<<< HEAD
 import { AccessoutenteComponent } from './accessoutente/accessoutente.component';
-=======
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//angular material 
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { ProvaComponent } from './prova/prova.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
->>>>>>> ced062d7fd7360c5b4684343d3e175fbd8e5544a
 
 
 
@@ -35,13 +24,8 @@ import {MatIconModule} from '@angular/material/icon';
     CreazioneordineComponent,
     GestioneordineComponent,
     PagamentoComponent,
-<<<<<<< HEAD
-    AccessoutenteComponent,
-
-=======
     ProvaComponent,
      
->>>>>>> ced062d7fd7360c5b4684343d3e175fbd8e5544a
   ],
   imports: [
     BrowserModule,
--- src/app/orderlist/order.ts
+++ src/app/orderlist/order.ts
@@ -1,4 +1,20 @@
+import { MetodoPagamento } from "../enums/metodo-pagamento";
+import { FasciaOraria } from "../enums/fascia-oraria";
+
 export interface Order {
-    id_ordine: BigInt;
-    indirizzo_partenza: string;
+    idOrdine: BigInt;
+    idSpedizione: BigInt;
+    indirizzoPartenza: string;
+    indirizzoConsegna: string;
+    volumeSpedizione: number;
+    pesoSpedizione: number;
+    numTelefonoDestinatario: String;
+    metodoPagamento: MetodoPagamento;
+    fasciaOraria: FasciaOraria;
+    noteConsegna: string;
+    costoFinale: number;
+    longitudinePartenza: number;
+    latitudinePartenza: number;
+    longitudineDestinazione: number;
+    latitudineDestinazione: number;
   }

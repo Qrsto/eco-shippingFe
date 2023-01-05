151cba523c2a6b554548c23fc617a639a12941f9
PC-DOMENICO\dcarb <d.carbonemsn00@libero.it>
2023-01-05 17:45:49 +0100

    test component angular material

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prova.component.css']
 })
 export class ProvaComponent implements OnInit {
+
+  cani = [{
+    nome: 'Sally',
+    razza: 'Non si capisce molto',
+    descrizione: `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+    from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+    originally bred for hunting.`
+  }]
   ngOnInit(): void {
       
   }

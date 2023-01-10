60cbcf6087d518c39831e412d520e8c81568bfbd
PC-DOMENICO\dcarb <d.carbonemsn00@libero.it>
2023-01-10 19:14:54 +0100

    update gestione ordine service

--- src/app/services/gestioneordine.service.ts
+++ src/app/services/gestioneordine.service.ts
@@ -1,11 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Gestioneordine } from '../models/gestioneordine.model';
+
+const baseUrl = 'http://localhost:8080/ordine'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GestioneordineService {
 
+  constructor(private http: HttpClient) { }
 
+  get(id: any): Observable<Gestioneordine> {
 
-  constructor() { }
+    return this.http.get(`${baseUrl}/${id}`)
+
+  }
+
+  getAll(): Observable<Gestioneordine[]> {
+
+    return this.http.get<Gestioneordine[]>(baseUrl);
+
+  }
+  //Inserisco le funzioni che voglio
+
 }

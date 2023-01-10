324010a3dc85c4fc04c683837c20dcd50c698ab3
PC-DOMENICO\dcarb <d.carbonemsn00@libero.it>
2023-01-10 13:31:36 +0100

    update Bug Fix: correzione import orderlist.css e altro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { CustomerListComponent } from 'src/app/components/customerlist/customerlist';
-import { OrderlistComponent } from 'src/app/components/orderlist/orderlist';
+import { OrderlistComponent } from 'src/app/components/orderlist/orderlist.component';
 import { CreazioneordineComponent } from './components/creazioneordine/creazioneordine.component';
 import { GestioneordineComponent } from './components/gestioneordine/gestioneordine.component';
 import { PagamentoComponent } from './components/pagamento/pagamento.component';
 
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -28,6 +30,9 @@ import { PagamentoComponent } from './components/pagamento/pagamento.component';
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
 
   ],
   providers: [],

853d2adc1e99771ffcc837d8e6633c59862be62b
PC-DOMENICO\dcarb <d.carbonemsn00@libero.it>
2023-01-12 13:31:36 +0100

    inizio impl pagamento

--- src/app/components/pagamento/pagamento.component.ts
+++ src/app/components/pagamento/pagamento.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order } from '../orderlist/order';
 
 @Component({
   selector: 'app-pagamento',
   templateUrl: './pagamento.component.html',
   styleUrls: ['./pagamento.component.css']
 })
-export class PagamentoComponent {
+export class PagamentoComponent implements OnInit {
+  ngOnInit(): void {
+      
+  }
 
-}
+  form : any = {
+    cardNumber: null,
+    dataScadenza: null,
+    cvvNumber: null,
+    titolareCarta: null,
+
+  };
+  errorMessage = '';
+  isSuccessful = false;
+  isCreatePaymentFailed = false;
+
+  constructor() {}
+
+  onSubmit(): void {
+    
+  }
+
+  
+    
+    
+
+  }
+
+
+
+
+
